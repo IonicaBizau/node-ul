a43593067863faac5ce21cef2a0579622a66cd89
Ionică Bizău <bizauionica@yahoo.com>
2014-12-21 11:15:10 +0200

    Added test dir

---
+++ test/index.js
@@ -0,0 +1,14 @@
+var Ul = require("../lib");
+
+var obj = {
+        n: null
+      , v: 1
+    }
+  , def = {
+        n: 1
+      , v: 10
+      , a: 20
+    }
+  ;
+
+console.log(Ul.merge(obj, def));
